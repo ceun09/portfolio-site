1c6c801231a0b88ebf3f9af1dd87c4a056c72a19
ceun09 <103122147+ceun09@users.noreply.github.com>
2022-05-24 20:13:12 +0900

    Slick Slider 수정
    theme-slider : 버튼이 작동하지 않던 오류 수정

--- portfolio/website-works/03/assets/plugin/slick-slider.js
+++ portfolio/website-works/03/assets/plugin/slick-slider.js
@@ -36,6 +36,7 @@ $(document).ready(function () {
     arrows: true,
     prevArrow:"<button type='button' class='slick-prev'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='slick-next'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    draggable: false
   })
 
   /* Media Video Slider */
